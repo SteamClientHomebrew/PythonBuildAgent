bb65abbc622e2b4d9cb83ea8bd02549bff4c3b3c
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-07-28 00:55:48 -0300

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -30,10 +30,9 @@ New-Item -ItemType Directory -Path "./python-build" -Force
 
 Write-Host "Generating delay libraries for Python 3.11.8..."
 
-../scripts/dlltool.exe --version
-
-../scripts/dlltool.exe --input-def ../exports.def --output-delaylib "./python-build/python311.lib" --dllname "./PCbuild/win32/python311.dll"
-../scripts/dlltool.exe --input-def ../exports.def --output-delaylib "./python-build/python311_d.lib" --dllname "./PCbuild/win32/python311_d.dll"
+cmd /c "..\scripts\dlltool.exe --version"
+cmd /c "..\scripts\dlltool.exe --input-def ..\exports.def --output-delaylib ./python-build/python311.lib --dllname ./PCbuild/win32/python311.dll"
+cmd /c "..\scripts\dlltool.exe --input-def ..\exports.def --output-delaylib ./python-build/python311_d.lib --dllname ./PCbuild/win32/python311_d.dll"
 
 Write-Host "Done!"
 
